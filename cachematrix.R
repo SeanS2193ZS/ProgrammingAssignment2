b196d430f66d3763f021910e865782893d9b4c31
SeanS2193ZS <Sean.Summers@ZS.com>
2017-05-07 14:22:10 -0400

    Summers Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+### R Programming 
+### Week 3 - Programming Assignment
+### SEAN S - 05.01.2017
+### DIR: /Users/Sean/Documents/R Files/ZS ADS/00 R Programming/Week 3/W3 Assignment"
 
-## Write a short comment describing this function
+## setwd('/Users/Sean/Documents/R Files/ZS ADS/00 R Programming/Week 3/W3 Assignment/ProgrammingAssignment2') 
+## getwd()
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## makeCacheMatrix | Accepts a matrix - and can perform 4 functions on the matrix it is provided 
+## (set, get, setInverse, getInverse)
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+  
+  matrixInverse <- NULL
+  
+  set <- function(y) 
+  {
+    x <<- y
+    matrixInverse <<- NULL
+  }
+  
+  get <- function() x
+
+  setInverse <- function(inverse) matrixInverse <<- inverse
+
+  getInverse <- function() matrixInverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+## cacheSolve | Accepts a matrix, checks to see if the inverse has already been cached - if so -
+## returns the cached value - if not cacluclated - calls the setInverse function to calculate
+
+cacheSolve <- function(x) {
+  matrixInverse <- x$getInverse() 
+  if(!is.null(matrixInverse)) { 
+    message("getting cached data")
+    return(matrixInverse)
+  }
+  
+  data <- x$get()  
+  matrixInverse <- solve(data) 
+  x$setInverse(matrixInverse)  
+  matrixInverse               
 }
 
 
-## Write a short comment describing this function
+### TEST CODE
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Sample run:
+x = rbind(c(0, -1), c(-1/2, 0))
+m = makeCacheMatrix(x)
+m$get()
+
+cacheSolve(m)
+cacheSolve(m)
